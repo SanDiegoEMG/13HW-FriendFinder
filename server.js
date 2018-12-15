2b12e37cf9570e7f2281a7c778c9babf03e7744d
SD_emg <sandiegoemg@gmail.com>
2018-12-15 15:17:09 -0800

    connecting with server - not showing anything

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+var express = require("express");
+var app = express();
+var path = require("path");
+
+
+// Express server configuration - set-up basic principles
+var app = express();  // Tells node that we are creating an "express" server
+
+var PORT = process.env.PORT || 3000;  // Listen for and set default port
+
+// Sets up the Express to accept the following data forms from POST requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Connect with other .js files so their code is accessble
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+
+// LISTENER - need one of these with any server program - effectively "starts" the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
