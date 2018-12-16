e6e6401d1e5f684206fb0c7aff72ee19971c718b
SD_emg <sandiegoemg@gmail.com>
2018-12-15 16:30:10 -0800

    got the api post and get working ... still all from one page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,6 @@
 var path = require("path");
 
+// Connect to .js with friend array in it
+var friendArray = require("../data/friends");
+
+
--- server.js
+++ server.js
@@ -14,23 +14,23 @@ app.use(express.json());
 
 
 // Data =====================================================
-var friends = [ 
+var friends = [
   {
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
     "scores": [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
-},
+  },
 ]
 // Data =====================================================
 
@@ -40,17 +40,37 @@ require("./app/routing/apiRoutes");
 require("./app/routing/htmlRoutes");
 
 
-// Routing to html page files
-    app.get("/home", function(req, res) {
-      res.sendFile(path.join(__dirname, "./app/public/home.html"));
-    });
+// Routing users to html pages
+app.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
 
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-    });
+app.get("/survey", function (req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
 
+// Data (api) routes
+
+
+app.get("/api/friends", function (req, res) {
+  res.json(friends);
+});
+
+
+app.post("/api/friends", function(req, res) {
+  var newPerson = req.body;
+  console.log(newPerson);
+  friends.push(newPerson);
+  res.json(newPerson);
+});
+
+
+// catch all for anything not specifically declared above
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
 
 // LISTENER - need one of these with any server program - effectively "starts" the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
